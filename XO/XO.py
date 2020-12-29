aef277ed7fbf15fcb060faecd5f52532745678f5
mohammed ibrahem <toti_alfy@outlook.com>
2020-12-29 03:08:00 +0200

    second commit

--- XO/XO.py
+++ XO/XO.py
@@ -59,6 +59,49 @@ def turning(player):
  
   board_game[position] = player
   show_board()
+def if_win_with_row():
+  
+  global game_still_going
+  
+  row_1 = board_game[0] == board_game[1] == board_game[2] != "-"
+  row_2 = board_game[3] == board_game[4] == board_game[5] != "-"
+  row_3 = board_game[6] == board_game[7] == board_game[8] != "-"
+  
+  if row_1 or row_2 or row_3:
+    still_going = False
+  
+  if row_1:
+    return board[0] 
+  elif row_2:
+    return board[3] 
+  elif row_3:
+    return board[6] 
+
+  else:
+    return None
+
+
+
+def if_win_with_columns():
+  
+  global still_going
+  
+  column_1 = board_game[0] == board_game[3] == board_game[6] != "-"
+  column_2 = board_game[1] == board_game[4] == board_game[7] != "-"
+  column_3 = board_game[2] == board_game[5] == board_game[8] != "-"
+ 
+  if column_1 or column_2 or column_3:
+    still_going = False
+ 
+  if column_1:
+    return board[0] 
+  elif column_2:
+    return board[1] 
+  elif column_3:
+    return board[2] 
+  
+  else:
+    return None
 
   # ------------ Start Execution -------------
 play_game()
